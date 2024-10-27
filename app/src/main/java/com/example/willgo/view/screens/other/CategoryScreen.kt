b0a1b6b8d9bf838f01d934be3ffcdabef60c6a3b
DBlaZ <carmonaborja2003@gmail.com>
2024-10-27 11:18:25 +0100

    unused imports

--- app/src/main/java/com/example/willgo/view/screens/other/CategoryScreen.kt
+++ app/src/main/java/com/example/willgo/view/screens/other/CategoryScreen.kt
@@ -1,7 +1,5 @@
 package com.example.willgo.view.screens.other
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,12 +8,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -27,7 +23,6 @@ import androidx.navigation.NavHostController
 import com.example.willgo.data.Category
 import com.example.willgo.data.Event
 import com.example.willgo.view.sections.CommonEventCard
-import com.example.willgo.view.sections.EventCard
 
 @Composable
 fun CategoryScreen(onBack: () -> Unit, category: Category, events: List<Event>, navController: NavHostController){
--- app/src/main/java/com/example/willgo/view/sections/EventSection.kt
+++ app/src/main/java/com/example/willgo/view/sections/EventSection.kt
@@ -22,7 +22,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.example.willgo.graphs.HomeScreenRoutes
 
